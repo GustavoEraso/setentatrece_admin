7200c99bd6a5d7d778e7ea4ea16efd2a5c4f7512
Gustavo Eraso <gustavoeraso1988@gmail.com>
2022-09-30 04:30:52 -0300

    arrancando la web para gestionar los pedidos,por ahora solo html y css

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+let datos ;
+
+const btn_ordenesPendientes= document.querySelector('.btn-ordenes-pendientes');
+btn_ordenesPendientes.addEventListener('click', function(){
+   
+})
+const btn_ordenesParaReparto= document.querySelector('.btn-ordenes-para-reparto');
+btn_ordenesPendientes.addEventListener('click', function(){
+   
+})
+const btn_ordenesEntregadas= document.querySelector('.btn-ordenes-para-reparto');
+btn_ordenesPendientes.addEventListener('click', function(){
+   
+})
